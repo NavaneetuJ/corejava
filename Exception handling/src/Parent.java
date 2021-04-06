dba4238a0e51fd5e6651052e644af222766e1362
Navaneetu <Navaneetu@LAPTOP-IIPJCH1C>
2021-04-06 21:46:44 +0530

    Exception handling

---
+++ Exception handling/src/Parent.java
@@ -0,0 +1,15 @@
+import java.io.*;  
+public class Parent {
+	void msg(){System.out.println("parent");}  
+}  
+
+class TestExceptionChild extends Parent{  
+	void msg()throws IOException{  
+		System.out.println("TestExceptionChild");  
+	}  
+	public static void main(String args[]){  
+		Parent p=new TestExceptionChild();  
+		p.msg();  
+	}  
+
+}

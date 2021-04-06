7f9d1684136cc4324285fbc738f5d074b5fc6d11
Navaneetu <Navaneetu@LAPTOP-IIPJCH1C>
2021-04-07 00:06:51 +0530

    Java date and time

---
+++ Date Time/src/OffsetDateTimeExample6.java
@@ -0,0 +1,9 @@
+import java.time.OffsetDateTime; 
+public class OffsetDateTimeExample6 {
+	public static void main(String[] args) {  
+		OffsetDateTime offset = OffsetDateTime.now();  
+		OffsetDateTime value =  offset.plusDays(240);  
+		System.out.println(value);  
+	}
+
+}

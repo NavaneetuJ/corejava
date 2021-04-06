771a0ede06b9f75f5044ae3f22c2a5ed74de53e4
Navaneetu <Navaneetu@LAPTOP-IIPJCH1C>
2021-04-06 21:24:14 +0530

    Abstraction , Polymorphism, Array, Encapsulation

---
+++ CLASS/src/com/javapoint/Test.java
@@ -0,0 +1,10 @@
+package com.javapoint;
+
+public class Test {
+	public static void main(String[] args){
+		Student s=new Student();
+		s.setName("Navaneetu J");
+		System.out.println(s.getName());  
+	}
+
+}

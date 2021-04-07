2bd46d56793eea4906b51589ce27b9c2c1d07c66
Navaneetu <Navaneetu@LAPTOP-IIPJCH1C>
2021-04-07 17:22:56 +0530

    serialization

---
+++ serialization/src/serialization/Receiver.java
@@ -0,0 +1,18 @@
+package serialization;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
+public class Receiver {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		FileInputStream fis = new FileInputStream("zoo.txt");
+		ObjectInputStream ois = new ObjectInputStream(fis);
+
+		Tiger tiger= (Tiger)ois.readObject();
+		System.out.println("Tiger variables are" +tiger.a);
+
+	}
+
+}
